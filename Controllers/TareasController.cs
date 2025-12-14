8f71b287bac108ba936330b7486b0a5670429e3e
Marcos <marcosmartostuvi@gmail.com>
2025-12-14 17:39:49 -0300

    crear metodo post en tareascontroller

--- Controllers/TareasController.cs
+++ Controllers/TareasController.cs
@@ -10,15 +10,36 @@ namespace TodoList.Controllers
     {
 
         [HttpGet]
-        public void Tareita()
+        public ActionResult<IEnumerable<TareaResponseDto>> TodasLasTareas()
         {
-            Console.WriteLine("Estoy funcionando");
+            var tareaEjemplo = new List<TareaResponseDto>()
+            {
+                new TareaResponseDto {Id = 1, Titulo = "Titulo 1", Descripcion = "descripcion 1", EstaCompleta = false},
+                new TareaResponseDto { Id = 2, Titulo = "Titulo 2", Descripcion = "descripcion 2", EstaCompleta=false},
+            };
+
+            return Ok(tareaEjemplo);
+           
         }
+        
 
+        // Solicitud POST
         [HttpPost]
-        public Task<IEnumerable<CrearTareaRequestDto>> CrearTarea(CrearTareaRequestDto tareaDto)
+        public ActionResult<TareaResponseDto> CrearTarea([FromBody] CrearTareaRequestDto tareaDto)
         {
+            Console.WriteLine($"Título: {tareaDto.Titulo}");
+            Console.WriteLine($"Descripción: {tareaDto.Descripcion}");
 
+
+            var respuesta = new TareaResponseDto()
+            {
+                Id = new Random().Next(100, 1000),
+                Titulo = tareaDto.Titulo,
+                Descripcion = tareaDto.Descripcion,
+                EstaCompleta = false,
+            };
+
+            return StatusCode(StatusCodes.Status201Created, respuesta);
         }
      
     }
--- Services/TareasServices.cs
+++ Services/TareasServices.cs
@@ -1,39 +1,40 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
-using TodoList.DTOs;
+﻿//using Microsoft.AspNetCore.Http.HttpResults;
+//using TodoList.DTOs;
 
-namespace TodoList.Services
-{
+//namespace TodoList.Services
+//{
 
-    // Cumplimos con los requisitos de la interfaz ITareaService
-    public class Tarea
-    {
-        public int Id { get; set; }
-        public string? Titulo { get; set; }
-        public string? Descripcion { get; set; }
-        public bool EstaCompleta { get; set; } = false;
-    }
-    public class TareasServices : ITareaService
-    {
-        private readonly List<Tarea> _tareas = new List<Tarea>();
-        private int _nextId = 1; // Para simular la generación de ID
+//    // Cumplimos con los requisitos de la interfaz ITareaService
+//    public class Tarea
+//    {
+//        public int Id { get; set; }
+//        public string? Titulo { get; set; }
+//        public string? Descripcion { get; set; }
+//        public bool EstaCompleta { get; set; } = false;
+//    }
+//    public class TareasServices : ITareaService
+//    {
+//        private readonly List<Tarea> _tareas = new List<Tarea>();
+//        private int _nextId = 1; // Para simular la generación de ID
 
-        public async Task<TareaResponseDto> CrearTarea(CrearTareaRequestDto tareaDto)
-        {
-            // Validar dto
-            if (string.IsNullOrEmpty(tareaDto.Titulo))
-            {
-                return BadRequest(); ;
-            }
-            // Pasar info del dto a Dominio
-            var nuevaTarea = new Tarea
-            {
-                Id: _nextId++,
-                Titulo = tareaDto.Titulo,
-                Descripcion = tareaDto.Descripcion,
-                EstaCompleta = false
-            };
+//        public async Task<TareaResponseDto> CrearTarea(CrearTareaRequestDto tareaDto)
+//        {
+//            // Validar dto
+//            if (string.IsNullOrEmpty(tareaDto.Titulo))
+//            {
+//                return BadRequest(); ;
+//            }
+//            // Pasar info del dto a Dominio
+//            var nuevaTarea = new Tarea
+//            {
+//                Id: _nextId++,
+//                Titulo = tareaDto.Titulo,
+//                Descripcion = tareaDto.Descripcion,
+//                EstaCompleta = false
+//            }
+//         };
 
-        }
+//        }
 
-    }
-}
+//    }
+//}
