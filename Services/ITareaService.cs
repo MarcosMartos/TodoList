0e72e14e66d29379c5cb95d100225969d3b76f14
Marcos <marcosmartostuvi@gmail.com>
2025-12-15 14:21:03 -0300

    crear contrato en interfaz ITareasServices

--- Services/ITareaService.cs
+++ Services/ITareaService.cs
@@ -4,7 +4,13 @@ namespace TodoList.Services
 {
     public interface ITareaService
     {
-        Task<TareaResponseDto> CrearTarea(CrearTareaRequestDto tareaDto);
+        // Método para crear una tarea
+        Task<TareaResponseDto> CrearTareaAsync(CrearTareaRequestDto tareaDto);
 
+        // Método para buscar una tarea por id
+        Task<TareaResponseDto?> ObtenerTareaPorIdAsync(int id);
+
+        // Método para buscar todas las tareas
+        Task<IEnumerable<TareaResponseDto>> ObtenerTodasLasTareasAsync();
     }
 }
